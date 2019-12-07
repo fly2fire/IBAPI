9cb3f2883094113172fe2f4651cbbdd64d70cd83
Trent Crawley <trentcrawley@gmail.com>
2019-12-07 16:42:00 +1100

    config example

---
+++ configExample.py
@@ -0,0 +1,9 @@
+class config():
+
+    def __init__(self):
+        self.dataBaseName = 'enter database name'
+        self.user = 'postgres'
+        self.password = 'enter password'
+
+
+x = config()
