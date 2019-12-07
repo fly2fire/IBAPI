17ab070b90befb19bb28240c7c533a0c00489242
Trent Crawley <trentcrawley@gmail.com>
2019-12-07 16:41:45 +1100

    Connect to Postgres DB

---
+++ dbconnect.py
@@ -0,0 +1,44 @@
+import psycopg2
+import psycopg2.extras
+import configreal
+
+config = configreal.config()
+class connectdb():
+    def pgconnect(self):
+        try:
+            self.conn = psycopg2.connect(host='localhost',
+                                    database=config.dataBaseName,
+                                    user=config.user,
+                                    password=config.password)
+            print('connected')
+        except Exception as e:
+            print("unable to connect to the database")
+            print(e)
+        return self.conn
+
+    def pgquery(self, conn, sqlcmd, args, silent=False, returntype='tuple'):
+       """ utility function to execute some SQL query statement
+           it can take optional arguments (as a dictionary) to fill in for placeholder in the SQL
+           will return the complete query result as return value - or in case of error: None
+           error and transaction handling built-in (by using the 'with' clauses) """
+       retval = None
+       with self.conn:
+          cursortype = None if returntype != 'dict' else psycopg2.extras.RealDictCursor
+          with self.conn.cursor(cursor_factory=cursortype) as cur:
+             try:
+                if args is None:
+                    cur.execute(sqlcmd)
+                else:
+                    cur.execute(sqlcmd, args)
+                retval = cur.fetchall() # we use fetchall() as we expect only _small_ query results
+             except Exception as e:
+                if e.pgcode != None and not(silent):
+                    print("db read error: ")
+                    print(e)
+       return retval
+
+    def disconnect(self):
+        self.conn.close()
+        print('connection closed')
+
+
